0f98856b356cc25d04f38df73df48b7ecfa8e1c6
hjhwanggg <hhyejeong30@gmail.com>
2021-04-02 17:53:20 +0900

    Add Customrer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell'; 
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 import { render } from 'react-dom';
 
@@ -18,16 +19,21 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing.unit * 2
   }
 });
 
 class App extends Component {
 
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   }
 
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
       .then(res => this.setState({customers:res}))
       .catch(err => console.log(err));
@@ -37,6 +43,11 @@ class App extends Component {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1});
   }
 
   render() {
@@ -67,7 +78,13 @@ class App extends Component {
               job={c.job}
             />
             );
-          }) : "" }
+          }) :
+          <TableRow>
+            <TableCell colSpan="6" align="center">
+              <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+            </TableCell>
+          </TableRow>
+          }
           </TableBody>
         </Table>
       </Paper>
@@ -76,3 +93,23 @@ class App extends Component {
 }
 
 export default withStyles(styles)(App);
+
+
+/*
+// 컴포넌트 라이프사이클
+
+1) constructor()
+
+2) componentWillMount() : 컴포넌트 마운트되기 전
+
+3) render() : 실제로 컴포넌트 화면에 그리기
+
+4) componentDidMount() : 화면에 컴포넌트 그린 후 
+
+*/
+
+/*
+
+props or state => shouldComponentUpdate() : props나 state가 변경되면 실질적 다시 렌드 함수 불러와서 뷰 갱신
+
+*/
